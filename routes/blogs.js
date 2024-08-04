55b28fbf9bd33cd30685065330d1d98be73833cf
Fatih <yildizafatih@gmail.com>
2024-08-04 15:23:16 +0300

    refactor and more routes

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,18 +2,33 @@ const express = require("express");
 const router = express.Router();
 const Blog = require("../models/Blog");
 
-router.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+router.get("/", async (req, res) => {
+  const blogs = await Blog.find({});
+  res.json(blogs);
 });
 
-router.post("/", (request, response) => {
-  const blog = new Blog(request.body);
+router.post("/", async (req, res) => {
+  const blog = new Blog(req.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const result = await blog.save();
+  res.status(201).json(result);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { likes } = req.body;
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    id,
+    { likes: likes },
+    { new: true }
+  );
+  res.json(updatedBlog);
+});
+
+router.delete("/:id", async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(200).json({ message: "Blog deleted successfully" });
 });
 
 module.exports = router;
